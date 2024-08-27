405dd406ea60e8a696763b98d5cf6f36fddf720c
01candanedo <01candanedo@gmail.com>
2024-08-27 20:42:14 +0000

    add: javascript redirection

--- js/closeSesion.js
+++ js/closeSesion.js
@@ -1,14 +1,13 @@
-<script>
-        const menuIcon = document.querySelector('.menu-icon');
-        const menu = document.querySelector('.menu');
+const menuIcon = document.querySelector('.menu-icon');
+const menu = document.querySelector('.menu');
 
-        menuIcon.addEventListener('click', () => {
-            menu.classList.toggle('active');
-        });
+menuIcon.addEventListener('click', () => {
+    menu.classList.toggle('active');
+});
 
-        function cerrarSesion() {
-            // Aquí iría la lógica real de cierre de sesión
-            alert('Sesión cerrada. Redirigiendo al inicio...');
-            window.location.href = 'index.html';
-        }
-    </script>
+function cerrarSesion() {
+    // Aquí iría la lógica real de cierre de sesión
+    alert('Sesión cerrada. Redirigiendo al inicio...');
+    console.log("holaaaa")
+    window.location.href = 'index.html';
+}
