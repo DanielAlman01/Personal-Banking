a35fe091c73e46f9e6150f5dcf4ae96957deb474
Nanu <manuelnunez2629@gmail.com>
2024-08-27 19:38:00 +0000

    add: script prueba

---
+++ script.js
@@ -0,0 +1,18 @@
+document.getElementById('form-log').addEventListener('submit', function(event) {
+  event.preventDefault  
+  const correctemail = 'Usuariocorrecto';
+  const passwordemail = 'Passwordcorrecta';
+
+  const enteredemail = document.getElementById('emailLogin').value;
+  const enteredpassword = document.getElementById('passwordLogin').value;
+
+  if(enteredemail == correctemail && passwordemail == enteredpassword){
+    alert('Inicio de sesion exitoso');
+
+  }
+  else{
+    const errorMessage = document.getElementById('error message');
+    errorMessage.textContent = 'Usuario o contraseña incorrectos';
+    errorMessage.style.display = 'block';
+  }
+});
