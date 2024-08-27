0172013cba0c389c1815aeec92333a62bdd42e2c
Daniel Almanza <daniel.almanza3@utp.ac.pa>
2024-08-27 20:40:17 +0000

    Funcion con el JS

---
+++ js/login.js
@@ -0,0 +1,15 @@
+
+const storedUser = 'Daniellamnza';
+const storedPassword = 'DanPPP2003';
+
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const email = document.getElementById('emailLogin').value;
+    const password = document.getElementById('passwordLogin').value;
+
+    if (email === storedUser && password === storedPassword) {
+        window.location.href = '../registro.html';
+    } else {
+        alert('El usuario o la contraseña es incorrecta');
+    }
+});
