df46c6050dbbbd5d017c761792ec11083f0c5875
Waldoox <kbarria903@gmail.com>
2024-08-28 19:01:30 +0000

    haciendo redirecciones

---
+++ js/registro.js
@@ -0,0 +1,4 @@
+document.getElementById('formularioRegistro').addEventListener('submit', function(event) {
+    event.preventDefault(); // Evita el envío del formulario
+    window.location.replace('Productos.html'); // Redirige a producto.html
+});
